99b854215f6f1902721b8ac5d56e41247668d966
udit01 <uditjainiitd@gmail.com>
2017-05-27 21:40:28 +0530

    added apm and npm set functionality

--- set_proxy.sh
+++ set_proxy.sh
@@ -6,3 +6,11 @@ export https_proxy="https://proxy22.iitd.ac.in:3128"
 export HTTP_PROXY="http://proxy22.iitd.ac.in:3128"
 export HTTPS_PROXY="https://proxy22.iitd.ac.in:3128"
 gsettings set org.gnome.system.proxy mode 'auto'
+
+# apm proxy
+apm config set proxy $http_proxy
+apm config set https_proxy $https_proxy
+
+# npm proxy
+npm config set proxy $http_proxy
+npm config set https-proxy $https_proxy
